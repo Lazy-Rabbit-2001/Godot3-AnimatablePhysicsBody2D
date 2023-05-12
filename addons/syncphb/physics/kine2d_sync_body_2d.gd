b50175250fa90a0d0238fc614f771e25580654ef
Lazy-Rabbit-2001 <2733679597@qq.com>
2023-05-12 17:51:04 +0800

    Update kine2d_sync_body_2d.gd

--- addons/syncphb/physics/kine2d_sync_body_2d.gd
+++ addons/syncphb/physics/kine2d_sync_body_2d.gd
@@ -31,24 +31,19 @@ func _physics_process(delta: float) -> void:
 			var collis: Array
 			var scl: Vector2 = scale
 			
-			var t: float = Time.get_ticks_usec()
 			var coll: KinematicCollision2D = move_and_collide(Vector2.UP.rotated(global_rotation), true, true, true)
 			if coll:
 				var collider: PhysicsBody2D = coll.collider as PhysicsBody2D
 				if !collider in collis:
 					collis.append(collider)
-			print(Time.get_ticks_usec() - t)
 			
-			print(collis)
-			for j in collis:
-				if j is SyncKinematicBody2D && j.is_on_floor() && !j.is_on_wall() && !j.is_on_ceiling():
-					var vel: Vector2 = constant_linear_velocity + global_position.direction_to(j.position).tangent() * deg2rad(constant_angular_velocity)
-					var floor_velocity: Vector2 = j.get_floor_velocity()
-					if leaving_behavior == LeavingBehavior.ALWAYS || (leaving_behavior == LeavingBehavior.UPWARD_ONLY && floor_velocity.dot(j.up_direction) > 0):
-						j.velocity += vel - j._velocity
-						if "motion_lock" in j:
-							j.motion_lock = true
-#						if j is DynamicsBody2D:
-#							j.motion = j.velocity.rotated(-global_rotation)
+			for i in collis:
+				if i is SyncKinematicBody2D && i.is_on_floor() && !i.is_on_wall() && !i.is_on_ceiling():
+					var vel: Vector2 = constant_linear_velocity + global_position.direction_to(i.position).tangent() * deg2rad(constant_angular_velocity)
+					var floor_velocity: Vector2 = i.get_floor_velocity()
+					if leaving_behavior == LeavingBehavior.ALWAYS || (leaving_behavior == LeavingBehavior.UPWARD_ONLY && floor_velocity.dot(i.up_direction) > 0):
+						i.velocity += vel - i._velocity
+						if "motion_lock" in i:
+							i.motion_lock = true
 		
 		set(SETTING, true)
